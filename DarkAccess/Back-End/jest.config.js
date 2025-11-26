fa78cae60e7f59ae613e373528de841bee03dc6e
luanjc77 <costa.luanjacomini@gmail.com>
2025-11-25 22:42:34 -0300

    backend testes

---
+++ DarkAccess/Back-End/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'routes/**/*.js',
+    '!routes/**/index.js',
+    '!**/node_modules/**',
+    '!**/tests/**'
+  ],
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  }
+};
